57b58d998cc4acf2fbc4fbcdc8853d804395a820
arshadhs <arshadhs@gmail.com>
2024-09-21 15:37:59 +0100

    update school script

--- school/school.py
+++ school/school.py
@@ -31,6 +31,8 @@ __copyright__ = "Free to all"
 
 import sys
 import datetime
+import argparse
+
 from openpyxl import load_workbook, Workbook
 from openpyxl.styles import PatternFill, Font, Alignment
 
@@ -110,13 +112,11 @@ def process_bank_st(filename, start, end):
             if not is_donor(row[:7]):
                 continue
             donor = desc[0]
-#            print ("donor", donor)
+ #           print ("donor", donor)
 
             # remove ' from the start of Description - Donor
             if (donor[0] == "'"): donor = donor[1:]
-#            print ("donor", donor)
-
-#            print("month: ", entry_time.month)
+#           print ("donor", donor)
 
             # Add a new DONOR to list, or add the amount to already existing DONOR
             if donor not in donors:
@@ -128,7 +128,7 @@ def process_bank_st(filename, start, end):
                     donors[donor]['total'+str(entry_time.month)] = float(value)
                 # if timestamp > donors[donor]['last donation']:
                     # donors[donor]['last donation'] = timestamp
-                print("Donor: ", donor)
+                #print("Donor: ", donor)
     print(process_bank_st.__name__ + " === END ===")
     return donors
 
@@ -140,13 +140,12 @@ def process_reference_data(filename):
     sheet = get_reference_sheet(iwb)
     if sheet:
         reference_data = {}
-        print(type(reference_data))
-        for row in sheet.iter_rows(min_row=2, max_col=2, values_only=True):
+        for row in sheet.iter_rows(min_row=2, max_col=3, values_only=True):
             if row[0] is not None:
-                print (row)
-                fname, ac_name = row[:2]
+#                print (row)
+                fname, ac_name, numOfChildren = row[:3]
 
-                reference_data[ac_name.lower()] = {'fname':fname}
+                reference_data[ac_name.lower()] = {'fname':fname, 'numOfChildren': int(numOfChildren)}
             else:
                 break
 
@@ -158,15 +157,17 @@ def process_reference_data(filename):
 ########################################################################
 
 # Write to an xlsx
-def write_output(data, reference_data):
+def write_output(donors, reference_data, outPath):
     print(write_output.__name__ + " === START ===")
     wb = Workbook()
     wb['Sheet'].title = 'School Fee'
     ws = wb['School Fee']
-    
+    total = 0
+
     # Write header
     for col, x in enumerate(("Parent's Name", "Reference",
-                             "September", "October", "November", "December", "January", "February", "March", "April", "May", "June", "July", "August"), start=1):
+                            "September", "October", "November", "December", "January", "February", "March", "April", "May", "June", "July", "August",
+                            "Child(ren)", "Paid", "Due"), start=1):
             ws.cell(1, col).value = x
             ws.cell(1, col).font = Font(bold=True, color='FFFFFF')
             ws.cell(1, col).fill = PatternFill("solid", fgColor="000000")
@@ -175,16 +176,15 @@ def write_output(data, reference_data):
     row = 2 # row 1 is header
 
     output = {}
-
     list_of_lists = [] 
-
+    
     # Iterates over Name and Reference (data from reference sheet)
     for reference, ac_name in reference_data.items():
 #        print("reference_data ", reference, ac_name)
         fee_data = donors.get(reference.upper())
 #        print("fee_data: ", fee_data)
         first_name = ac_name['fname'] #
-
+              
 #       reference = reference
         total1 = fee_data.get('total1', 0) if (fee_data != None) else 0
         total2 = fee_data.get('total2', 0) if (fee_data != None) else 0
@@ -199,6 +199,8 @@ def write_output(data, reference_data):
         total11 = fee_data.get('total11', 0) if (fee_data != None) else 0
         total12 = fee_data.get('total12', 0) if (fee_data != None) else 0
 
+        total = total + total1 + total2 + total3 + total4 + total5 + total6 + total7 + total8 + total9 + total10 + total11 + total12
+
         # If already the fname exists, catenate reference
         if first_name not in output:
             output[first_name] = {'reference' : reference}
@@ -207,64 +209,99 @@ def write_output(data, reference_data):
             output[first_name] = {'reference' : output[first_name].get('reference') + " + " + reference}
 #            print("output[first_name].get('reference') = " + output[first_name].get('reference'))
 
-        position = next((i for i, row in enumerate(list_of_lists) if row[0] == first_name), None)
-
+        position = next((i for i, row in enumerate(list_of_lists) if row[0] == first_name), None)     
+        
         if position is not None:
             list_of_lists[position] = [first_name, output[first_name].get('reference'), 
-                                        list_of_lists[position][2]+total9, list_of_lists[position][3]+total10, list_of_lists[position][4]+total11, list_of_lists[position][5]+total12, 
-                                        list_of_lists[position][6]+total1, list_of_lists[position][7]+total2, list_of_lists[position][8]+total3, list_of_lists[position][9]+total4, 
-                                        list_of_lists[position][10]+total5, list_of_lists[position][11]+total6, list_of_lists[position][12]+total7, list_of_lists[position][13]+total8]
+                                        list_of_lists[position][2]+total9,
+                                        list_of_lists[position][3]+total10,
+                                        list_of_lists[position][4]+total11,
+                                        list_of_lists[position][5]+total12, 
+                                        list_of_lists[position][6]+total1,
+                                        list_of_lists[position][7]+total2,
+                                        list_of_lists[position][8]+total3,
+                                        list_of_lists[position][9]+total4, 
+                                        list_of_lists[position][10]+total5,
+                                        list_of_lists[position][11]+total6,
+                                        list_of_lists[position][12]+total7,
+                                        list_of_lists[position][13]+total8,
+                                        ac_name['numOfChildren'], 0, 0
+                                       ]
         else:
-            masterdata = [first_name, output[first_name].get('reference'), total9, total10, total11, total12, total1, total2, total3, total4, total5, total6, total7, total8]
+            masterdata = [first_name, output[first_name].get('reference'), total9, total10, total11, total12, total1, total2, total3, total4, total5, total6, total7, total8, ac_name['numOfChildren'], 0, 0]
             list_of_lists.append(masterdata)
 
-#        print("masterData: ", masterData)
-#        print (type(first_name))
-#        print(type(total1))
-        # Map the reference from reference data to the bank st reference, and fetch the particular row
-        # for col, x in enumerate((first_name, output[first_name].get('reference'), total9, total10, total11, total12, total1, total2, total3, total4, total5, total6, total7, total8), start=1):
-            # if isinstance(x, datetime.datetime):    # If x is Date Time
-# #                print("XYZ: ", x)
-                # ws.cell(row, col).value = x.strftime('%d-%b-%Y')
-                # ws.cell(row, col).alignment = Alignment(horizontal='right')
-            # else:                                   # Print anything else in the row
-# #                print("abc: ", row, col, x)
-                # if (x == 0):
-                    # ws.cell(row, col).font = Font(bold=True, color='000000')
-                    # ws.cell(row, col).fill = PatternFill("solid", fgColor="D3D3D3")
-                # ws.cell(row, col).value = x
-
+    # Output to xlsx
     for r in list_of_lists: 
         column = 0
+        total = 0
+        due = 0
+#        print(r)
         for element in r:
-            column += 1
-            ws.cell(row, column).value = element
-            if (element == 0):
+            column += 1          
+            
+            if (column > 2 and column < 15):
+                total += element
+            if column == 15:
+                numOfChildren = element
+                ws.cell(row, column).value = element
+            if column == 16:
+                ws.cell(row, column).value = total
+            elif column == 17:               
+                amountToBePAid = 480 if numOfChildren == 1 else 900 if numOfChildren == 2 else 1080
+                due = amountToBePAid - total                
+                ws.cell(row, column).value = due if due > 0 else 0
+                if (due > 0):
                     ws.cell(row, column).font = Font(bold=True, color='000000')
                     ws.cell(row, column).fill = PatternFill("solid", fgColor="D3D3D3")
+
+            else:
+                ws.cell(row, column).value = element
+                if (element == 0):
+                        ws.cell(row, column).font = Font(bold=True, color='000000')
+                        ws.cell(row, column).fill = PatternFill("solid", fgColor="D3D3D3")
 
         row = row+1
 
     #print (output)
 #    print("list_of_lists: ", list_of_lists)
     print("Saving School Fee summary in output.xlsx")
-    wb.save('output.xlsx')
+    outFileName = 'output.xlsx' if outPath is None else outPath + "\\" 'output.xlsx'
+    wb.save(outFileName)
     print(write_output.__name__ + " === END ===")
 
 ########################################################################
 
-# C:\GA>school.py NatWest-download-2022-23-School.xlsx school.xlsx 2022
-if __name__ == '__main__':
-    bankStatementXLS, referenceXLS, _start = sys.argv[1:]
+def main():
+    parser = argparse.ArgumentParser(description='Process school arguments.')
+    parser.add_argument('--bank', dest='bankStatementXLS', default=None, help='Bank Statement')
+    parser.add_argument('--reference', dest='referenceXLS', default=None, help='Reference Statement')
+    parser.add_argument('--year', dest='year', default=datetime.datetime.now().year - 1, help='Year of school records')
 
-    start = datetime.date(int(_start), 9, 1)#4, 5)
-    end = datetime.date(int(_start) + 1, 8, 31)#4, 4)
+    parser.add_argument('--outputPath', dest='outPath', help='XLS file for timetable')
+
+    args = parser.parse_args()
+
+    bankStatementXLS = args.bankStatementXLS
+    referenceXLS = args.referenceXLS
+    year = args.year
+    outPath = args.outPath
+
+#    bankStatementXLS, referenceXLS, _start = sys.argv[1:]
+
+    start = datetime.date(int(year), 9, 1)#4, 5)
+    end = datetime.date(int(year) + 1, 8, 31)#4, 4)
     print(start)
     print(end)
     
     reference_data = process_reference_data(referenceXLS)
     donors = process_bank_st(bankStatementXLS, start, end)
 
-    write_output(donors, reference_data)
+    write_output(donors, reference_data, outPath)
+    
+
+# C:\GA>school.py NatWest-download-2022-23-School.xlsx school.xlsx 2022
+if __name__ == '__main__':
+    main()
 
 ########################################################################
