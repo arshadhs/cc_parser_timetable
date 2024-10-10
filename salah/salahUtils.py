bece92570bb3fb1493382dd836cc5b7310b6d414
Arshad Siddiqui <arshadhs@gmail.com>
2024-10-10 12:49:44 +0100

    Update salahUtils.py

--- salah/salahUtils.py
+++ salah/salahUtils.py
@@ -8,8 +8,9 @@ __author__      = "Arshad H. Siddiqui"
 __copyright__   = "Free to all"
 
 import configparser
+import math
 
-from datetime import datetime, time, timedelta
+from datetime import datetime, time, timedelta, date
 from zoneinfo import ZoneInfo # Python 3.9+
 
 # Parse the config file
@@ -39,3 +40,58 @@ def getDSTtransitionDates(year):
             dstTransitionDates.append((datetime(year, 1, 1) + timedelta(d)).date())
             print("DST Transition Sunday: ", (datetime(year, 1, 1) + timedelta(d)).date())
     return dstTransitionDates
+
+def add_and_ceil_dt(dt, add_minutes, delta):
+        # Reduce the time by the specified reduction in minutes
+        add_time = increment_time_by_minutes_dt(dt, add_minutes)
+
+        # Calculate the number of minutes since midnight
+        minutes = add_time.hour * 60 + add_time.minute
+        
+        # Always round up to the nearest delta minutes
+        rounded_minutes = math.ceil(minutes / delta) * delta
+        
+        # Calculate the new hour and minute
+        new_hour = rounded_minutes // 60
+        new_minute = rounded_minutes % 60
+
+        # Return the original date with the rounded up time
+        final_time = dt.replace(hour=int(new_hour), minute=new_minute, second=0, microsecond=0)
+        #print("reduce_and_floor_dt: ", dt, final_time)
+        return final_time
+
+def reduce_and_floor_dt(dt, reduction_minutes, delta):
+        # Reduce the time by the specified reduction in minutes
+        reduced_time = reduce_time_by_minutes_dt(dt, reduction_minutes)
+
+        # Calculate the number of minutes since midnight for the reduced time
+        minutes = reduced_time.hour * 60 + reduced_time.minute
+        
+        # Round down to the nearest delta minutes
+        rounded_minutes = math.floor(minutes / delta) * delta
+
+        # Calculate the new hour and minute
+        new_hour = rounded_minutes // 60
+        new_minute = rounded_minutes % 60
+        
+        # Return the reduced date with the rounded down time
+        final_time = reduced_time.replace(hour=int(new_hour), minute=int(new_minute), second=0, microsecond=0)
+        #print("reduce_and_floor_dt: ", dt, final_time)
+        return final_time
+        
+def reduce_time_by_minutes_dt(dt, reduction_minutes):
+    reduced_time = (datetime.combine(date(1,1,1), dt) - timedelta(minutes = reduction_minutes)).time()
+    return reduced_time
+
+def increment_time_by_minutes_dt(dt, reduction_minutes):
+    reduced_time = (datetime.combine(date(1,1,1), dt) + timedelta(minutes = reduction_minutes)).time()
+    return reduced_time
+
+def diff_in_minutes(dt1, dt2):
+   # convert into datetimes
+   dt1 = datetime.combine(datetime.now(), dt1)
+   dt2 = datetime.combine(datetime.now(), dt2)
+   # compute difference
+   delta = dt1 - dt2        # <--- datetime.timedelta(seconds=38417)
+   diff_in_minutes = int(delta.total_seconds()//60)
+   return diff_in_minutes
