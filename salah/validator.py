7c13e2be24146960537aa9cacbe03b314f0c246e
Arshad Siddiqui <arshadhs@gmail.com>
2024-10-10 12:50:27 +0100

    Update validator.py

--- salah/validator.py
+++ salah/validator.py
@@ -7,13 +7,55 @@
 __author__      = "Arshad H. Siddiqui"
 __copyright__   = "Free to all"
 
+import datetime
+import salahUtils
+
 def validateJamatTime(salahTable):
+
+    print("""
+Validating:
+    Fajr start and jamat time difference (to allow for Sunah)
+    Fajr jamat time gap from Sunrise
+    Maghrib start and jamat time difference
+    Isha jamat start and jamat time difference
+    Jamat time is after Booking time (if Jamat)
+    """)
+
     for row, (date, time) in enumerate(salahTable['schedule'].items(), start=1): # For each day in the year
-        # Fajr jamat time is after start time
-        if (time['Fajr'].start > time['Fajr'].jamat):
-           print("Error:", date, time['Fajr'].name, time['Fajr'].start, time['Fajr'].jamat)
-        if (time['Fajr'].start == time['Fajr'].jamat):
-           print("Warning:", date, time['Fajr'].name, time['Fajr'].start, time['Fajr'].jamat)
+
+        # Fajr jamat time difference from start
+        diffInMin = salahUtils.diff_in_minutes(time['Fajr'].jamat, time['Fajr'].start)
+
+        if (diffInMin < 5):
+           print("Error:", date, time['Fajr'].name,
+                   time['Fajr'].start,
+                   time['Fajr'].jamat,
+                   time['Fajr'].sunrise,
+                   diffInMin)
+        elif (diffInMin < 15):
+           print("Warning:", date, time['Fajr'].name,
+                   time['Fajr'].start,
+                   time['Fajr'].jamat,
+                   time['Fajr'].sunrise,
+                   diffInMin)
+
+
+
+        # Fajr jamat time gap from Sunrise
+        diffInMin = salahUtils.diff_in_minutes(time['Fajr'].sunrise, time['Fajr'].jamat)
+
+        if (diffInMin < 37):   #(salahUtils.reduce_time_by_minutes_dt(time['Fajr'].sunrise, 37)) < time['Fajr'].jamat):
+           print("Error (sunrise):", date, time['Fajr'].name,
+                   time['Fajr'].start,
+                   time['Fajr'].jamat,
+                   time['Fajr'].sunrise,
+                   salahUtils.diff_in_minutes(time['Fajr'].sunrise, time['Fajr'].jamat))
+        # elif (diffInMin < 45):   #(salahUtils.reduce_time_by_minutes_dt(time['Fajr'].sunrise, 37)) < time['Fajr'].jamat):
+           # print("Warning (sunrise):", date, time['Fajr'].name,
+                   # time['Fajr'].start,
+                   # time['Fajr'].jamat,
+                   # time['Fajr'].sunrise,
+                   # salahUtils.diff_in_minutes(time['Fajr'].sunrise, time['Fajr'].jamat))
 
         # Maghrib jamat time is after start time
         if (time['Maghrib'].jamat):
@@ -24,4 +66,19 @@ def validateJamatTime(salahTable):
         if (time['Isha'].start > time['Isha'].jamat):
            print("Error:", date, time['Isha'].name, time['Isha'].start, time['Isha'].jamat)
         elif (time['Isha'].start == time['Isha'].jamat):
-           print("Warning:", date, time['Isha'].name, time['Isha'].start, time['Isha'].jamat)           
+           print("Warning:", date, time['Isha'].name, time['Isha'].start, time['Isha'].jamat)
+
+        # Jamat time is after Booking time
+        if (time['Fajr'].booking_start > time['Fajr'].jamat):
+            print("Error (Fajr Booking):", date, time['Fajr'].name, time['Fajr'].booking_start, time['Fajr'].jamat)
+
+        if (time['Dhuhr'].jamat):
+            if (time['Dhuhr'].booking_start > time['Dhuhr'].jamat):
+                print("Error (Dhuhr Booking):", date, time['Dhuhr'].name, time['Dhuhr'].booking_start, time['Dhuhr'].jamat)
+
+        if (time['Maghrib'].jamat):
+            if (time['Maghrib'].booking_start > time['Maghrib'].jamat):
+                print("Error (Maghrib Booking):", date, time['Maghrib'].name, time['Maghrib'].booking_start, time['Maghrib'].jamat)
+
+        if (time['Isha'].booking_start > time['Isha'].jamat):
+            print("Error (Isha Booking):", date, time['Isha'].name, time['Isha'].booking_start, time['Isha'].jamat)
