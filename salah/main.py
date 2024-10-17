20541c56bbf7dc4e2009bc1c12d5d90f7a66e2b8
arshadhs <arshadhs@gmail.com>
2024-10-17 19:01:47 +0100

    cleanup

--- salah/main.py
+++ salah/main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 r"""
-    Implements Logic to build up a Salah Planner using data from moonsighting (url or xlsx)
+    Implements Logic to build a Salah Planner using data from moonsighting (URL or xlsx).
     
-    e.g.
-    python get_salah_timetable.py --year 2025 --file docs\salah2025.xlsx --usage web
-    C:\GitHub\cc\salah>python get_salah_timetable.py --year 2025 --file docs\salah2025.xlsx --usage booking hide
+    Usage example:
+    python get_salah_timetable.py --year 2025 --file docs/salah2025.xlsx --usage web
+    C:\GitHub\cc\salah>python get_salah_timetable.py --year 2025 --file docs/salah2025.xlsx --usage booking hide
 """
 
-__author__      = "Mohammad Azim Khan, Arshad H. Siddiqui"
-__copyright__   = "Free to all"
+__author__ = "Mohammad Azim Khan, Arshad H. Siddiqui"
+__copyright__ = "Free to all"
 
 import datetime
 import argparse
@@ -18,26 +18,24 @@ from openpyxl import Workbook
 from openpyxl.styles import PatternFill, Font, Alignment
 from openpyxl.styles.borders import Border, Side
 
+import xlsx_writer
+import csv_writer
 import utils
-
 from moon_sighting import get_prayer_table, get_prayer_table_offline
 from salah_object import Salah, recalculate_jamat_time
 from xlsx_generator import SalahWorkBook, FajrSalahWorkBook
 from validator import validateJamatTime
-import xlsx_writer #import writeXlsx
-import csv_writer
+
 
 COLOUR_BLUE = "add8e6"
-# COLOR_P_BLUE = "1e7ba0"
-# COLOR_S_BLUE = "0a2842"
-
 COLOUR_GREY = "dbdbdb"
 COLOR_L_GREY = "6e6e6e"
-# COLOR_D_GREY = "474747"
 
-# Build a Salah Object for time['x']
-# it swaps out time['x'] from 'time' to 'Salah' object
+# Create Salah object for each prayer time in the timetable
 def salah_gen(table):
+    """
+    Builds a Salah object for each prayer time in the timetable.
+    """
     for row, (date, time) in enumerate(table['schedule'].items(), start=1): # For each day in the year
         # print("time.values(): ", time.keys(), " : ", time.values())
                                     # odict_keys(['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'])
@@ -48,6 +46,7 @@ def salah_gen(table):
 
         Fajr, Sunrise, Dhuhr, Asr, Maghrib, Isha = time.values()
 
+        # Create Salah objects with specified jamat settings
         time['Fajr'] = Salah('Fajr', date, Fajr, time, has_jamat=True)
         time['Dhuhr'] = Salah('Dhuhr', date, Dhuhr, time, has_jamat=True)
         time['Asr'] = Salah('Asr', date, Asr, time, has_jamat=False)
@@ -56,14 +55,20 @@ def salah_gen(table):
 #    print (table)
     return table
 
-
+# Calculate and adjust Salah times based on daylight saving time (DST)
 def salah_calculator(salahTable, dstDates):
+    """
+    Recalculates Jamat times for Salah objects based on DST adjustments.
+    """
     salahReCalcTable = recalculate_jamat_time(salahTable, dstDates)
     return salahTable # salahReCalcTable
 
 
-# Build WorkBook Cells for each prayer time
+# Build the workbook cells for each prayer time in the timetable
 def workbook_gen(table):
+    """
+    Generates a workbook for Salah timetable data.
+    """
     for row, (date, time) in enumerate(table['schedule'].items(), start=1): # For each day in the year
 #        print ("date: ", date)      # date:  2025-12-29
 #        print ("time: ", time)      # time:  OrderedDict({
@@ -71,6 +76,7 @@ def workbook_gen(table):
                                     # 'Dhuhr': <salah.Salah object at 0x0463C198>, 'Asr': <salah.Salah object at 0x0463C2A0>,
                                     # 'Maghrib': <salah.Salah object at 0x0463C3C0>, 'Isha': <salah.Salah object at 0x0463C4C8>})
         Fajr, Sunrise, Dhuhr, Asr, Maghrib, Isha = time.values()
+
         time['Fajr'] = FajrSalahWorkBook(time['Fajr'], usage)
         time['Dhuhr'] = SalahWorkBook(time['Dhuhr'], usage)
         time['Asr'] = SalahWorkBook(time['Asr'], usage)
@@ -83,6 +89,7 @@ def workbook_gen(table):
 usage = ""
 hideColumns = False
 
+# Main function to parse arguments and generate the Salah timetable
 def main():
     parser = argparse.ArgumentParser(description='Generate Salah Time Table for booking and web.')
     parser.add_argument('--year', dest='year', default=datetime.datetime.now().year, help='Year of salah timetable')
@@ -103,32 +110,26 @@ def main():
     # ramadan_start, ramadan_end = get_ramadan_dates(int(args.year))
     # print(f"Ramadan in {args.year} starts on {ramadan_start} and ends on {ramadan_end}")
 
-    # using config file
+    # Get Ramadan dates from config file
     ramadan_start, ramadan_end = utils.get_config("config.ini", args.year)
     print(f"Ramadan Start ({args.year}): {ramadan_start}")
     print(f"Ramadan End ({args.year}): {ramadan_end}")
 
-    # If the filename is supplied fetch the data from file, else from URL
-    if args.filename is not None:
-        moonSightTable = get_prayer_table_offline(args.year, args.filename)
-    else:
-        table = get_prayer_table(args.year)
-
-    # If the filename is supplied fetch the data from file, else from URL
+    # Fetch moon sighting data from file or URL
     if args.year is not None:
         moonSightTable = get_prayer_table_offline(args.year, args.filename)
     else:
         moonSightTable = get_prayer_table(args.year)
 
-    # Add booking data to moon sighting data
+    # Generate Salah objects
     salahBookingTable = salah_gen(moonSightTable)
     dstDates = utils.getDSTtransitionDates(int(args.year))
     salahCalculatedTable = salah_calculator(salahBookingTable, dstDates)
 
-    # Validate the calculated booking data
+    # Validate calculated Jamat times
     validateJamatTime(salahCalculatedTable)
 
-    # Generate the xlsx
+    # Generate the output file based on usage type
     if (args.usage == "booking"):
         wbTable = workbook_gen(salahCalculatedTable)
         xlsx_writer.not_in_use(args.filename)
